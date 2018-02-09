bab0bb9c219c4b51ae674e13ada3d56474e6c3ec
Matt Herman <mfherman@gmail.com>
2018-02-09 10:09:27 -0500

    mayor and 2014 cong map

--- scripts/util-make-shp-cd11.R
+++ scripts/util-make-shp-cd11.R
@@ -3,6 +3,7 @@ library(here)
 library(janitor)
 library(sf)
 
+## 2017
 # define vector of eds in cd11
 cd11_eds_2017 <- read_csv(
   here("data/cd11_eds_2017.csv"),
@@ -17,4 +18,24 @@ read_sf(here("data/geo/nyed_17c")) %>%
   filter(elect_dist %in% cd11_eds_2017) %>%
   select(elect_dist) %>%
   st_transform(4326) %>%
-  st_write("output/geo/cd11_ed_2017.geojson")
+  st_write(here("output/geo/cd11_ed_2017.geojson"))
+
+
+## 2014
+# define vector of eds in cd11
+cd11_eds_2014 <- read_csv(
+  here("output/elect_results/2014_cd11.csv"),
+  col_types = cols(.default = "c")
+  ) %>%
+  pull(elect_dist)
+
+# select only cd11 eds and write to geojson
+read_sf(here("data/geo/nyed_14c")) %>%
+  clean_names() %>%
+  mutate(elect_dist = as.character(elect_dist)) %>%
+  filter(elect_dist %in% cd11_eds_2014) %>%
+  select(elect_dist) %>%
+  st_transform(4326) %>%
+  st_write(here("output/geo/cd11_ed_2014.geojson"))
+
+hi <- read_sf(here("output/geo/cd11_ed_2014.geojson"))
